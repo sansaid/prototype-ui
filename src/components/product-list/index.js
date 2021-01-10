9e3cfb88602d3b189e417edef7664644ad629ca9
Sanyia Saidova <sanyia.saidova@gmail.com>
2021-01-10 00:07:01 +0000

    Changing class based components to functional

--- src/components/product-list/index.js
+++ src/components/product-list/index.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
 import { products } from './products';
 import './product-list.css';
 
-export default class ProductList extends Component {
-    share() {
+export default function ProductList () {
+    function share() {
         window.alert('The product has been shared!');
     }
 
-    getProduct(product, index) {
+    function getProduct(product, index) {
         return (
             <div key={index}>
                 <h3>
                     <a title={product.name + ' details'} href="/">{product.name}</a>
                 </h3>
                 <p>Description: {product.description} </p>
-                <button className="btn" onClick={this.share}>Share</button>
+                <button className="btn" onClick={share}>Share</button>
             </div>
         )
     }
 
-    render() {
-        return (
-            <>
-                <h2>Products</h2>
-                <br/>
-                <div>
-                    {
-                        products.map((product, index) => {
-                            return this.getProduct(product, index);
-                        })
-                    }
-                </div>
-            </>
-        );
-    }
+    return (
+        <>
+            <h2>Products</h2>
+            <br/>
+            <div>
+                {
+                    products.map((product, index) => {
+                        return getProduct(product, index);
+                    })
+                }
+            </div>
+        </>
+    )
 }
