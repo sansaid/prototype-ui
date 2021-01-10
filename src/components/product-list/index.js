812a3ba81c8d40c5ab0665fd44061b73a131d5cb
Sanyia Saidova <sanyia.saidova@gmail.com>
2021-01-10 00:42:33 +0000

    Fixing build errors

--- src/components/product-list/index.js
+++ src/components/product-list/index.js
@@ -1,23 +1,24 @@
+import React from 'react';
 import { products } from './products';
 import './product-list.css';
 
-export default function ProductList () {
-    function share() {
-        window.alert('The product has been shared!');
-    }
+function share() {
+    window.alert('The product has been shared!');
+}
 
-    function getProduct(product, index) {
-        return (
-            <div key={index}>
-                <h3>
-                    <a title={product.name + ' details'} href="/">{product.name}</a>
-                </h3>
-                <p>Description: {product.description} </p>
-                <button className="btn" onClick={share}>Share</button>
-            </div>
-        )
-    }
+function getProduct(product, index) {
+    return (
+        <div key={index}>
+            <h3>
+                <a title={product.name + ' details'} href="/">{product.name}</a>
+            </h3>
+            <p>Description: {product.description} </p>
+            <button className="btn" onClick={share}>Share</button>
+        </div>
+    )
+}
 
+export default function ProductList() {
     return (
         <>
             <h2>Products</h2>
--- src/components/top-bar/index.js
+++ src/components/top-bar/index.js
@@ -1,5 +1,6 @@
+import React from 'react';
 
-export default function TopBar() {
+export default function TopBar(props) {
     return (
         <div className={props.className}>
             <a href="/">
